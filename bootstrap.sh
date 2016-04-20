aed092d6a4f4f9bf7311b698ee283629d73da3a7
nerutenbeck <nerutenbeck@gmail.com>
2016-04-20 13:31:18 -0400

    Move install scripts to one directory, rename based on OS

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "osx" --exclude "ubuntu" \
+		--exclude "README.md" --exclude "install" \
         -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
