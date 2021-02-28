0a2aeb8e2d892eb5cbd105fc99539ef4b74eb8fb
Nathan Rutenbeck <nathan@stonesetfarm.com>
2021-02-27 23:27:17 -0500

    Minimal set of updated R packages

--- install/R_pkgs.R
+++ install/R_pkgs.R
@@ -1,17 +1,11 @@
-#! /usr/bin/Rscript
+install.packages(
+  "tidyverse",
+  "devtools",
+  "sf",
+  "tidybayes",
+  "bayesplot",
+  "brms"
+)
 
-package_list <- c("argparser"
-                  "caret",
-                  "devtools",
-                  "dplyr",
-                  "ggplot2",
-                  "knitr",
-                  "pander",
-                  "readr",
-                  "roxygen2",
-                  "rmarkdown",
-                  "rstan",
-                  "tidyr")
-
-install.packages(package_list, 
-                 dependencies = c("depends", "suggests"))
+remotes::install_github("stan-dev/cmdstanr")
+remotes::install_github("stan-dev/posterior")
